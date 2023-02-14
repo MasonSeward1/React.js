0bf8e2ee643cb424357cfdb4c34d299d8065bab9
MasonSeward1 <mason.seward49@ed.cna.nl.ca>
2023-02-14 18:47:27 -0330

    Movie Changes

--- src/pages/MovieRatings.js
+++ src/pages/MovieRatings.js
@@ -3,8 +3,7 @@ import { useState, useEffect } from "react";
 const MovieRatings = () => {
     let [movies, setMovies] = useState(null);
     function removeMovie(id) {
-        const newList = movies.filter((item) => item.id !== id);
-        setMovies(newList);
+        setMovies(movies.filter((item) => item.id !== id));
     }
 
     useEffect( () => {
@@ -24,13 +23,15 @@ const MovieRatings = () => {
     return (
         <ul id="movie_display">
            {
-                movies.map(movie => [
-                <li key={movie.id}>Name: {movie.name}</li>, 
-                <li>Release Date: {movie.releaseDate}</li>, 
-                <li>Actors: {movie.actors}</li>,
-                <li id="poster"><img src={movie.poster} alt="movie poster"></img></li>, 
-                <li>Rating: {movie.rating}</li>, <button type="button" onClick = { () => {
-                    console.log("Movie Deleted"); removeMovie(movie.id)}}>Remove Movie</button>, <p></p>])
+                movies.map(movie => 
+                [
+                    <li key={movie.id}>Name: {movie.name}</li>, 
+                    <li>Release Date: {movie.releaseDate}</li>, 
+                    <li>Actors: {movie.actors}</li>,
+                    <li id="poster"><img src={movie.poster} alt="movie poster"></img></li>, 
+                    <li>Rating: {movie.rating}</li>, <button type="button" onClick = { () => {
+                        console.log("Movie Deleted"); removeMovie(movie.id)}}>Remove Movie</button>, <p></p>
+                ])
            }
         </ul>
     )
