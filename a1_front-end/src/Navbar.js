9a0760e17905ab5e4805b81a48fa1a9a44e648be
MasonSeward1 <110777088+MasonSeward1@users.noreply.github.com>
2023-02-22 17:23:51 -0330

    Delete a1_front-end directory

--- a1_front-end/src/Navbar.js
+++
@@ -1,9 +0,0 @@
-import {Link} from "react-router-dom";
-
-export default function Navbar() {
-    return (
-    <nav>
-        <li><Link to="/">Submitted Movie Ratings</Link> </li>
-        <li><Link to="/SubmitMovie">Submit a Movie Rating!</Link> </li>
-    </nav>);
-}
