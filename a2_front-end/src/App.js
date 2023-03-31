3bc0057de341625a064c1c702adc66344db22694
MasonSeward1 <mason.seward49@ed.cna.nl.ca>
2023-03-31 12:12:48 -0230

    Initial Commit for Project using assignment 2 as template. Removed code for gathering and loading movies in app.js. Installed dependencies

--- a2_front-end/src/App.js
+++ a2_front-end/src/App.js
@@ -3,27 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import SubmitMovieReview from './pages/FormPage';
 import MovieRatings from './pages/MovieRatings';
 import NavigationBar from './Navbar';
-import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-
-  let [movie, setMovie] = useState(null);
-
-  useEffect( () => {
-    fetch('/api/movies')
-    .then(reponse => reponse.json())
-    .then(setMovie)
-    .catch(e=>console.log(e.message))
-    }, [])
-
   return (
     <>
       <NavigationBar />
          <Routes>
-           <Route path="/" element={<MovieRatings movies={(movie)} setMovies={(setMovie)}/>} />
-           <Route path="/SubmitMovie" element={<SubmitMovieReview movies={(movie)} setMovies={(setMovie)}/>} />
+           <Route path="/" element={<MovieRatings />} />
+           <Route path="/SubmitMovie" element={<SubmitMovieReview />} />
          </Routes>
     </>
     );
